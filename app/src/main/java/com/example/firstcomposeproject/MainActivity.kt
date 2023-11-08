8050b516ce76b09c461867b7b04293529b66bc72
i.kurbankulov <ikurbankulov@gmail.com>
2023-11-08 16:49:02 +0600

    Initial commit

---
+++ app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.firstcomposeproject
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TimesTable()
+        }
+    }
+
+    @Preview
+    @Composable
+    fun TimesTable() {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            for (i in 1 until 10) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                ) {
+                    for (f in 1 until 10) {
+                        Box(
+                            modifier = Modifier
+                                .background(if ((i + f) % 2 == 0) Color.Green else Color.White)
+                                .fillMaxHeight()
+                                .weight(1f)
+                                .border(width = 1.dp, color = Color.Black),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(text = "${i * f}")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
